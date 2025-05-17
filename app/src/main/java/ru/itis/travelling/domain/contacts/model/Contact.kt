8d225f7ee987c7feff9cc621aafbd474964cc34d
Raliiinz <garifullinaralina@gmail.com>
2025-05-17 14:52:49 +0300

    change

--- app/src/main/java/ru/itis/travelling/domain/contacts/model/Contact.kt
+++ app/src/main/java/ru/itis/travelling/domain/contacts/model/Contact.kt
@@ -7,6 +7,6 @@ import kotlinx.parcelize.Parcelize
 data class Contact(
     val name: String?,
     val phoneNumber: String,
-    val isSelected: Boolean = false
+    val isSelected: Boolean = true
 ) : Parcelable
 /////
